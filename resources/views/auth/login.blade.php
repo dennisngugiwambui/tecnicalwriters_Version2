fee3aa024a38f3f4ad9a73fc6cabccb43ac25404
dennisngugiwambui <dellyit001@gmail.com>
2025-02-15 14:34:02 +0300

    update to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,10 +12,10 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="phone" class="col-md-4 col-form-label text-md-end">{{ __('Phone') }}</label>
 
                             <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone" autofocus>
+                                <input id="phone" type="number" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
